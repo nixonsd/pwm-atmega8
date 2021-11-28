a28dca364dbd42dacf7dc1d8e6864901ca990cff
Bogdan <bandanovvs@gmail.com>
2021-11-28 23:07:04 +0200

    Initial commit

---
+++ PWM Atmega8/main.c
@@ -0,0 +1,18 @@
+/*
+ * PWM Atmega8.c
+ *
+ * Created: 28.11.2021 22:58:13
+ * Author : banda
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
