a63dfeaa8e844a98aa3b2e532ee8a7cbc8ed79e2
Bogdan <bandanovvs@gmail.com>
2021-11-30 00:25:09 +0200

    Added Atmega8 microcontroller firmware

--- PWM Atmega8/main.c
+++ PWM Atmega8/main.c
@@ -1,18 +1,42 @@
 /*
- * PWM Atmega8.c
+ * Atmega8 microcontroller firmware 
  *
- * Created: 28.11.2021 22:58:13
- * Author : banda
+ * PWM Atmega8.c (main)
+ * Author : Kiselov
  */ 
 
+#define F_CPU 8000000UL
 #include <avr/io.h>
+#include <stdlib.h>
+#include <util/delay.h>
 
+void ADC_Init() {
+	ADMUX |= (1 << REFS0);
+	ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2) | (1 << ADEN);
+}
+
+uint16_t ADC_Read() {
+	ADCSRA |= (1 << ADSC);
+	return ADC;
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+	uint16_t brightness = 0x00;
+	DDRB= 1<<4; // Enable fourth b pin for output
+	PORTB=0x00; // Empty all b ports
+	ASSR=0x00; // Empty asynchronous status register
+	TCCR2=0b01101110; // Fast PWM, Clear OC2 on Compare Match, clkT2S/256 (From prescaler)
+	
+	// Init adc (start adc conversion)
+	ADC_Init();
+	
+    /* Main loop */
     while (1) 
     {
+		brightness = ADC_Read() * 0.249267;
+		OCR2 = brightness;
+		_delay_ms(100);
     }
 }
 
